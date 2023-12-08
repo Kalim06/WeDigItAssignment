36c1fd98c30c09d3550b09e9cb992bef6f039544
Kalim Dalwai <dalwaikalim1@gmail.com>
2023-12-08 19:30:59 +0530

    Login Design

---
+++ app/src/main/java/com/example/wedigitassignment/TextViewExtensions.kt
@@ -0,0 +1,24 @@
+package com.example.wedigitassignment
+
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+
+fun TextView.appendText(string: String?, @ColorRes color: Int) {
+    if (string.isNullOrEmpty()) {
+        return
+    }
+
+    val spannable: Spannable = SpannableString(string)
+    spannable.setSpan(
+        ForegroundColorSpan(ContextCompat.getColor(context, color)),
+        0,
+        spannable.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+
+    append(spannable)
+}
--- app/src/main/java/com/example/wedigitassignment/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/wedigitassignment/fragments/LoginFragment.kt
@@ -6,15 +6,39 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.wedigitassignment.R
+import com.example.wedigitassignment.appendText
+import com.example.wedigitassignment.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Set SignUp Text
+        binding.signUp.appendText("New to WeJammin? ", R.color.textColor)
+        binding.signUp.appendText("Sign up here", R.color.textBlueColor)
+
+        //Set Terms and Privacy Text
+        binding.termsAndPrivacyText.appendText(
+            "By entering, you agree to Wejammin’s ",
+            R.color.textColor
+        )
+        binding.termsAndPrivacyText.appendText("Privacy Policy ", R.color.textBlueColor)
+        binding.termsAndPrivacyText.appendText("and\n ", R.color.textColor)
+        binding.termsAndPrivacyText.appendText("Terms of Service", R.color.textBlueColor)
+
     }
 
 }
